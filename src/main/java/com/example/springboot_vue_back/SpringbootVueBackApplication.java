7bab4779680a47c414fbdf6f18514cbe47536f00
常兆海 <1341195069@qq.com>
2023-03-15 14:36:47 +0800

    空项目提交

---
+++ src/main/java/com/example/springboot_vue_back/SpringbootVueBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_vue_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVueBackApplication.class, args);
+    }
+
+}
