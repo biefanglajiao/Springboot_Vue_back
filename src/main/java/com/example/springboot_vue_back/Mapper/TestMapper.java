2b6250bd1ab571f249dbeb6147542d46d9d4b2fc
常兆海 <1341195069@qq.com>
2023-03-17 19:55:50 +0800

    集成D:mybatis-generator

--- src/main/java/com/example/springboot_vue_back/Mapper/TestMapper.java
+++ src/main/java/com/example/springboot_vue_back/Mapper/TestMapper.java
@@ -4,7 +4,7 @@ import com.example.springboot_vue_back.domain.Test;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-@Mapper
+//@Mapper  //因为有了MapperScan注解   所以就不需要写Mapper了
 public interface TestMapper {
     public List<Test> list();
 }
