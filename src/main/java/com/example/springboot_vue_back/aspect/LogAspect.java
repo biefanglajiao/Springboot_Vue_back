e3087dff60f23849ede1db014a94a48d9db48b3c
常兆海 <1341195069@qq.com>
2023-04-03 11:51:57 +0800

    SpringAop 统计接口耗时

---
+++ src/main/java/com/example/springboot_vue_back/aspect/LogAspect.java
@@ -0,0 +1,100 @@
+package com.example.springboot_vue_back.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.PropertyPreFilter;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//执行顺序  ： 过滤器 》 拦截器 》 切面
+@Aspect
+@Component
+public class LogAspect {//aop 模式 接口耗时
+private static final Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+//定义一个切点
+   @Pointcut("execution(public * com.example.springboot_vue_back.Controller.*.*(..))")//切点表达式 所有的controller方法
+    public void contorllerPointcut() {
+    }
+    @Before("contorllerPointcut()")//在切点之前执行
+    public void doBefore(JoinPoint joinPoint) throws Throwable{
+       //开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        //打印请求信息
+        LOG.info("------------- LogAspect 开始 -------------");
+        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("远程地址: {}", request.getRemoteAddr());
+        LOG.info("请求方法: {}.{}", signature.getDeclaringTypeName(), name);
+
+
+        //打印请求参数
+        Object[] args = joinPoint.getArgs();
+//        LOG.info("请求参数: {}", joinPoint.getArgs());
+
+        Object[] arguments = new Object[args.length];
+        for (int i=0;i<args.length;i++){
+            if (args[i] instanceof HttpServletRequest||args[i] instanceof HttpServletResponse||args[i] instanceof MultipartFile){
+               continue;
+        }arguments[i]=args[i];
+        }
+
+        //排除字段；敏感字段或者 太长的不显示
+        String[] excludeFields = {"password","newPassword","oldPassword","file"};
+        PropertyPreFilters filter = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludeFilter = filter.addFilter();
+        excludeFilter.addExcludes(excludeFields);
+        LOG.info("请求参数: {}", com.alibaba.fastjson.JSON.toJSONString(arguments,excludeFilter));
+
+
+    }
+
+    @Around("contorllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();//前置
+        Object result = proceedingJoinPoint.proceed();//执行方法
+        // 排除字段，敏感字段或太长的字段不显示
+        String[] excludeProperties = {"password", "file"};//后置
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+    /**
+     * 使用nginx做反向代理，需要用该方法才能取到真实的远程IP
+     * @param request
+     * @return
+     */
+    public String getRemoteIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
--- src/main/java/com/example/springboot_vue_back/filter/logFilter.java
+++ src/main/java/com/example/springboot_vue_back/filter/logFilter.java
@@ -1,40 +1,40 @@
-package com.example.springboot_vue_back.filter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-/***
- *  过滤器 用于记录日志 用于记录请求的地址  请求的时间  请求的耗时
- */
-@Component//将logFilter注入到spring容器中
-public class logFilter implements Filter {
-    private static final Logger LOG = LoggerFactory.getLogger(logFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("logFilter init");
-    }
-
-    @Override//过滤器 用于记录日志 用于记录请求的地址  请求的时间  请求的耗时
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        LOG.info("dofilter开始");
-        LOG.info("请求地址：{}{}", request.getRequestURL().toString(), request.getMethod());
-        LOG.info("远程地址：{}", request.getRemoteAddr());
-
-        long startTIme = System.currentTimeMillis();
-        filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("耗时：{}ms", System.currentTimeMillis() - startTIme);
-    }
-
-
-    @Override
-    public void destroy() {
-        System.out.println("logFilter destroy");
-    }
-}
+//package com.example.springboot_vue_back.filter;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+///***
+// *  过滤器 用于记录日志 用于记录请求的地址  请求的时间  请求的耗时
+// */
+//@Component//将logFilter注入到spring容器中
+//public class logFilter implements Filter {
+//    private static final Logger LOG = LoggerFactory.getLogger(logFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        System.out.println("logFilter init");
+//    }
+//
+//    @Override//过滤器 用于记录日志 用于记录请求的地址  请求的时间  请求的耗时
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("dofilter开始");
+//        LOG.info("请求地址：{}{}", request.getRequestURL().toString(), request.getMethod());
+//        LOG.info("远程地址：{}", request.getRemoteAddr());
+//
+//        long startTIme = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        LOG.info("耗时：{}ms", System.currentTimeMillis() - startTIme);
+//    }
+//
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("logFilter destroy");
+//    }
+//}
