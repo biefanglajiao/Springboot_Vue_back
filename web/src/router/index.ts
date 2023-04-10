8460f5c596c99a1030f53367e3d5490e3088eda7
常兆海 <1341195069@qq.com>
2023-04-10 10:36:53 +0800

    电子书管理功能开发： 页面首字母小写 （规范）

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home.vue'//非惰性 全局加载、
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +10,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    /****
+     *@解释: route level code-splitting 路由级代码分解  this generates a separate chunk (about.[hash].js) for this route 这将为该路由生成一个单独的块(约.[hash].js)  which is lazy-loaded when the route is visited. 它在访问路由时是惰性加载的。
+     */
+        component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   }
 ]
 
