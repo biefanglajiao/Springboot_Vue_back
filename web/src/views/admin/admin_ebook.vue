232cc5410f9cfa726a7d5620144093509699af9a
常兆海 <1341195069@qq.com>
2023-04-11 21:54:28 +0800

    电子书管理功能开发： 编辑框的内容书写

--- web/src/views/admin/admin_ebook.vue
+++ web/src/views/admin/admin_ebook.vue
@@ -14,18 +14,18 @@
         >
           <template #cover="{ text:cover }">
             <img class="img_xhz" v-if="cover" :src="cover" alt="avatar">
-<!--            //todo 图片的处理-->
+            <!--            //todo 图片的处理-->
           </template>
-     <template v-slot:action="{text,record}">
-        <a-space size="small">
-          <a-button type="primary" @click="edit">
-            编辑
-          </a-button>
-          <a-button type="danger">
-            删除
-          </a-button>
-        </a-space>
-     </template>
+          <template v-slot:action="{text,record}">
+            <a-space size="small">
+              <a-button type="primary" @click="edit(record)">
+                编辑
+              </a-button>
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-space>
+          </template>
         </a-table>
 
 
@@ -33,15 +33,25 @@
     </a-layout>
   </a-layout-content>
   <a-modal title="电子书表单" v-model:visible="modalVisible" :confirm-loading="modalLoading" @ok="handleModalOk">
-    <template #footer>
-      <a-button key="back" @click="handleCancel">Return</a-button>
-      <a-button key="submit" type="primary" :loading="loading" @click="handleOk">Submit</a-button>
-    </template>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
+    <a-form :model="ebook" :label-col="{ span: 4 }" :wrapper-col="{ span: 14 }" :layout="formLayout">
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
+      </a-form-item>
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover"/>
+      </a-form-item>
+      <a-form-item label="分类一">
+        <a-input v-model:value="ebook.category1Id"/>
+      </a-form-item>
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id"/>
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.description" type="text"/>
+      </a-form-item>
+
+
+    </a-form>
   </a-modal>
 </template>
 
@@ -51,50 +61,51 @@ import axios from "axios";
 
 export default defineComponent({
   name: 'AdminEbook',
-  setup(){
-    const ebooks=ref();
-    const pagination=ref({
+  setup() {
+    const ebooks = ref();
+    const pagination = ref({
       current: 1,
       pageSize: 2,
-      total:0
+      total: 0
     });
-    const loading =ref(false);
-    const columns=[{
-      title:'封面',
-      dataIndex:'cover',
-    //   渲染
-      slots:{customRender:'cover'}
+    const loading = ref(false);
+    const columns = [{
+      title: '封面',
+      dataIndex: 'cover',
+      //   渲染
+      slots: {customRender: 'cover'}
     },
       {
-        title:'名称',
-        dataIndex:'name',
+        title: '名称',
+        dataIndex: 'name',
       },
-      { title:'分类一',
-        key:'category1Id',
-        dataIndex:'category1Id',
+      {
+        title: '分类一',
+        key: 'category1Id',
+        dataIndex: 'category1Id',
 
       },
       {
-        title:'分类二',
-        key:'category2Id',
-        dataIndex:'category2Id',
+        title: '分类二',
+        key: 'category2Id',
+        dataIndex: 'category2Id',
       },
       {
-        title:'文档数',
-        dataIndex:'docCount',
+        title: '文档数',
+        dataIndex: 'docCount',
       },
       {
-        title:'阅读数',
-        dataIndex:'docCount',
+        title: '阅读数',
+        dataIndex: 'docCount',
       },
       {
-        title:'点赞数',
-        dataIndex:'docCount',
+        title: '点赞数',
+        dataIndex: 'docCount',
       },
       {
-        title:'Action',
-        key:'action',
-        slots:{customRender:'action'}
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
       }
     ];
     /*****
@@ -102,37 +113,39 @@ export default defineComponent({
      */
     const modalLoading = ref<boolean>(false);
     const modalVisible = ref<boolean>(false);
-
+    const ebook = ref();
     const handleModalOk = () => {
-     modalLoading.value=true;
+      modalLoading.value = true;
       setTimeout(() => {
         modalLoading.value = false;
         modalVisible.value = false;
       }, 2000);
     };
 
-    const edit = () => {
-    modalVisible.value = true;
+    const edit = (record: any) => {
+      modalVisible.value = true;
+      ebook.value=record;
+      console.log(ebook.value);
     }
 
     /***
      * @方法描述: 数据查询方法
      */
-    const handleQuery=(params:any)=>{
-      loading.value=true;
-      axios.get("/ebook/list",{
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      axios.get("/ebook/list", {
         params: {
           page: params.page,
           size: params.size,
         }
-      }).then((response)=>{
-        loading.value=false;
-        const  data=response.data;
-        ebooks.value=data.content.list;
+      }).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        ebooks.value = data.content.list;
 
         //重置分页按钮
-        pagination.value.current=params.page;
-        pagination.value.total=data.content.total;
+        pagination.value.current = params.page;
+        pagination.value.total = data.content.total;
       });
     };
 
@@ -140,17 +153,17 @@ export default defineComponent({
     /***
      * @方法描述: 表格点击页面触发
      */
-    const handleTableChange = (pagination:any) => {
+    const handleTableChange = (pagination: any) => {
       handleQuery({
-        page:pagination.current,
-        size:pagination.pageSize
+        page: pagination.current,
+        size: pagination.pageSize
       });
 
     };
     /**
      * @方法描述: 初始进入页面就查一次数据
      */
-    onMounted(()=>{
+    onMounted(() => {
       handleQuery({
         page: 1,
         size: pagination.value.pageSize,
@@ -158,16 +171,17 @@ export default defineComponent({
     });
 
 
-    return{
+    return {
       ebooks,
       pagination,
       columns,
       loading,
       handleTableChange,
 
-    //   编辑相关
+      //   编辑相关
       modalLoading,
       modalVisible,
+      ebook,
       handleModalOk,
       edit
     }
