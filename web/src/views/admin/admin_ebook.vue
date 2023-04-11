0bc567b00ace98ef4911fa08fd5c28dac7edab2d
常兆海 <1341195069@qq.com>
2023-04-11 08:20:18 +0800

    电子书管理功能开发： Ant Design Vue表格组件的使用 修改电子书管理页面布局

--- web/src/views/admin/admin_ebook.vue
+++ web/src/views/admin/admin_ebook.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+
+  <a-layout-content style="padding: 0 50px">
+
+    <a-layout style="padding: 24px 0; background: #fff">
+
+      <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+        <div class="about">
+          <h1>This is an about page</h1>
+        </div>
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout-content>
 </template>
