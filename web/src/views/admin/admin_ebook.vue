a1aa975db2fdde3b61eb0b48c24cfd80ffc5e475
常兆海 <1341195069@qq.com>
2023-04-11 09:36:56 +0800

    电子书管理功能开发： Ant Design Vue表格组件的使用 数据页面与后端接口交互 分页

--- web/src/views/admin/admin_ebook.vue
+++ web/src/views/admin/admin_ebook.vue
@@ -5,11 +5,130 @@
     <a-layout style="padding: 24px 0; background: #fff">
 
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        <div class="about">
-          <h1>This is an about page</h1>
-        </div>
-        Content
+        <a-table :columns="columns"
+                 :data-source="ebooks"
+                 :row-key="record => record.id"
+                 :pagination="pagination"
+                 :loading="loading"
+                 @change="handleTableChange"
+        >
+          <template #cover="{ text:cover }">
+            <img v-if="cover" :src="cover" alt="avatar">
+<!--            //todo 图片的处理-->
+          </template>
+     <template v-slot:action="{text,record}">
+        <a-space size="small">
+          <a-button type="primary">
+            编辑
+          </a-button>
+          <a-button type="danger">
+            删除
+          </a-button>
+        </a-space>
+     </template>
+        </a-table>
+
+
       </a-layout-content>
     </a-layout>
   </a-layout-content>
 </template>
+
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue';
+import axios from "axios";
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup(){
+    const ebooks=ref();
+    const pagination=ref({
+      current: 1,
+      pageSize: 2,
+      total:0
+    });
+    const loading =ref(false);
+    const columns=[{
+      title:'封面',
+      dataIndex:'cover',
+    //   渲染
+      slots:{customRender:'cover'}
+    },
+      {
+        title:'名称',
+        dataIndex:'name',
+      },
+      { title:'分类一',
+        key:'category1Id',
+        dataIndex:'category1Id',
+
+      },
+      {
+        title:'分类二',
+        key:'category2Id',
+        dataIndex:'category2Id',
+      },
+      {
+        title:'文档数',
+        dataIndex:'docCount',
+      },
+      {
+        title:'阅读数',
+        dataIndex:'docCount',
+      },
+      {
+        title:'点赞数',
+        dataIndex:'docCount',
+      },
+      {
+        title:'Action',
+        key:'action',
+        slots:{customRender:'action'}
+      }
+    ];
+
+
+    /***
+     * @方法描述: 数据查询方法
+     */
+    const handleQuery=(params:any)=>{
+      loading.value=true;
+      axios.get("/ebook/list",params).then((response)=>{
+        loading.value=false;
+        const  data=response.data;
+        ebooks.value=data.content;
+
+        //重置分页按钮
+        pagination.value.current=params.page;
+      });
+    };
+
+
+    /***
+     * @方法描述: 表格点击页面触发
+     */
+    const handleTableChange = (pagination:any) => {
+      handleQuery({
+        page:pagination.current,
+        size:pagination.pageSize
+      });
+
+    };
+    /**
+     * @方法描述: 初始进入页面就查一次数据
+     */
+    onMounted(()=>{
+      handleQuery({});
+    });
+
+
+    return{
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange
+    }
+  }
+});
+</script>
