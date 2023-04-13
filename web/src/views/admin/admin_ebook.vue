2ae2e7b2ff16ca3bd3f5bfd8ff33cc5b874af8d8
常兆海 <1341195069@qq.com>
2023-04-13 22:20:51 +0800

    电子书管理功能开发： 新增搜索； 修改编辑时，不提交也会直接渲染 重新刷新后回复的问题 方法：  // ebook.value = record;改为          ebook.value=Tool.copy(record);          这句代码的含义是将record的值赋值给ebook.value，但是这样做会导致修改ebook.value的值的时候，record的值也会跟着改变，所以这里需要使用深拷贝的方式，将record的值赋值给ebook.value

--- web/src/views/admin/admin_ebook.vue
+++ web/src/views/admin/admin_ebook.vue
@@ -5,11 +5,21 @@
     <a-layout style="padding: 24px 0; background: #fff">
 
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+        <a-form layout="inline " :model="param" >
+        <a-space  direction="vertical">
+        <a-input-search
+            v-model:value="param.name"
+            placeholder="名称"
+            enter-button
+            @search="handleQuery({page:1,size:pagination.pageSize})"
+        />
+        </a-space>
         <p>
-          <a-button type="primary" @click="add()" size="large">
+        <a-button type="primary" @click="add()">
             新增
           </a-button>
         </p>
+        </a-form>
         <a-table :columns="columns"
                  :data-source="ebooks"
                  :row-key="record => record.id"
@@ -17,6 +27,8 @@
                  :loading="loading"
                  @change="handleTableChange"
         >
+          <!--        //Q::row-key="record => record.id这个代码的含义是什么-->
+          <!--        //A:row-key是一个属性，用来指定数据的主键，这里指定的是id，这样在表格中就可以通过id来唯一标识一行数据-->
           <template #cover="{ text:cover }">
             <img class="img_xhz" v-if="cover" :src="cover" alt="avatar">
             <!--            //todo 图片的处理-->
@@ -48,8 +60,8 @@
       </a-layout-content>
     </a-layout>
   </a-layout-content>
-  //Q::confirm-loading的含义
-  //A:confirm-loading是一个属性，当点击确定按钮时，会调用handleModalOk方法，handleModalOk方法会调用axios的post方法，保存数据，然后重新加载列表
+<!--  //Q::confirm-loading的含义-->
+<!--  //A:confirm-loading是一个属性，当点击确定按钮时，会调用handleModalOk方法，handleModalOk方法会调用axios的post方法，保存数据，然后重新加载列表-->
   <a-modal title="电子书表单" v-model:visible="modalVisible" :confirm-loading="modalLoading" @ok="handleModalOk">
     <a-form :model="ebook" :label-col="{ span: 4 }" :wrapper-col="{ span: 14 }" :layout="formLayout">
       <a-form-item label="名称">
@@ -77,10 +89,13 @@
 import {defineComponent, onMounted, ref} from 'vue';
 import axios from "axios";
 import {message} from "ant-design-vue";
+import { Tool } from '@/utils/tool';
 
 export default defineComponent({
   name: 'AdminEbook',
   setup() {
+    const param=ref();
+    param.value={};
     const ebooks = ref();
     const pagination = ref({
       current: 1,
@@ -157,8 +172,10 @@ export default defineComponent({
      */
     const edit = (record: any) => {
       modalVisible.value = true;
-      ebook.value = record;
-      console.log(ebook.value);
+      // ebook.value = record;
+      ebook.value=Tool.copy(record);
+      // Q:这句代码的含义
+      // A:这句代码的含义是将record的值赋值给ebook.value，但是这样做会导致修改ebook.value的值的时候，record的值也会跟着改变，所以这里需要使用深拷贝的方式，将record的值赋值给ebook.value
     }
     /***
      *@方法描述: 单击新增按钮方法
@@ -172,11 +189,7 @@ export default defineComponent({
     /***
      * @方法描述: 删除按钮方法
      */
-        //q: 这段代码的详细含义？
-        //a: 这段代码是删除按钮的方法，点击删除按钮后，会弹出一个确认框，确认后会调用delet方法，delet方法会调用axios的delete方法，删除数据，然后重新加载列表
-        //q:delet方法里的参数是什么意思？
-        //a:delet方法里的参数是id，id是数据库中的主键，用来唯一标识一条数据
-        //q: id:string 是什么意思？
+
 
     const delet = (id: number) => {
           axios.delete("/ebook/delete/" + id).then((response) => {
@@ -201,6 +214,7 @@ export default defineComponent({
         params: {
           page: params.page,
           size: params.size,
+          name: param.value.name,
         }
       }).then((response) => {
         loading.value = false;
@@ -243,11 +257,13 @@ export default defineComponent({
 
     return {
       //列表
+      param,
       ebooks,
       pagination,
       columns,
       loading,
       handleTableChange,
+      handleQuery,
 
       //   编辑表格相关
       modalLoading,
