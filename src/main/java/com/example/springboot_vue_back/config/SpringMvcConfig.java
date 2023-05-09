6e292e07617d3b91de3b0c6a37ee3c9e482168d9
常兆海 <1341195069@qq.com>
2023-05-09 10:25:55 +0800

    新增后端拦截器，需要校验token进行登录校验

--- src/main/java/com/example/springboot_vue_back/config/SpringMvcConfig.java
+++ src/main/java/com/example/springboot_vue_back/config/SpringMvcConfig.java
@@ -1,19 +1,31 @@
-//package com.example.springboot_vue_back.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import com.example.springboot_vue_back.interceptor.logInterceptor;
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//@Resource
-//    logInterceptor logInterceptor;
-//public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");//拦截所有请求，除了登录请求
-//    }
-//
-//}
+package com.example.springboot_vue_back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.example.springboot_vue_back.interceptor.logInterceptor;
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+@Resource
+    logInterceptor logInterceptor;
+
+    /***
+     * 添加拦截器  为什么用拦截器：因为他可以针对某些请求进行拦截，然后做一些处理
+     * @param registry
+     */
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/text/**",
+                        "/user/login",
+                        "category/all",
+                        "doc/all/**",
+                        "doc/find-content/**"
+                );//拦截所有请求，除了exculdePathPatterns中的请求
+    }
+
+}
