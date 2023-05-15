2b93a482c46f5372b71b1713dc217c5e5e17d3b9
常兆海 <1341195069@qq.com>
2023-05-15 14:29:08 +0800

    增加：  日志流水号 在日志中输出日志流水号        流水号是加在aop中的 定时任务不走aop 修改：  在定时任务中也添加流水号

--- src/main/java/com/example/springboot_vue_back/job/DocJob.java
+++ src/main/java/com/example/springboot_vue_back/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.example.springboot_vue_back.job;
 
 import com.example.springboot_vue_back.Mapper.DocMapperCust;
+import com.example.springboot_vue_back.Utils.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -21,12 +23,19 @@ import java.util.Date;
 @Resource
 private DocMapperCust   docMapperCust;
 
+@Resource
+private SnowFlake   snowFlake;
+
     /**
      * 自定义cron表达式跑批
      * 没60秒更新一次电子书的附属信息
      */
     @Scheduled(cron = "*/60 * * * * ?")
-    public void cron() throws InterruptedException {
+    public void cron()  {
+        //增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+
+
        LOG.info("更新电子书的附属信息：  开始 ");
 
        long start=System.currentTimeMillis();
